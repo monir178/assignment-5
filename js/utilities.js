24fe080061a8d0c6103d68e46198148b5be78da4
monir178 <monir.mzs17@gmail.com>
2022-08-23 20:47:42 +0600

    list problem solved

--- js/utilities.js
+++ js/utilities.js
@@ -1,19 +1,28 @@
 function selectPlayerById(playerNameId, playerBtnId) {
+
     const addSelected = document.getElementById('list-container');
     const name = document.getElementById(playerNameId);
     const li = document.createElement('li');
-
-
-
-
     li.innerText = name.innerText;
-    li.classList.add('item');
-    addSelected.appendChild(li);
-
-
 
     const btnName = document.getElementById(playerBtnId);
     btnName.setAttribute('disabled', true);
+
+
+    li.classList.add('item');
+    addSelected.appendChild(li);
+
+    let parents = document.querySelectorAll(".item");
+    let parentsLength = parents.length;
+
+    if (parentsLength === 6) {
+        alert('You can not add more than 5 players');
+
+        const remove = document.querySelector(".item")
+        // remove.lastChild.style.display = 'none';
+        remove.parentNode.removeChild(remove);
+
+    }
 
 
 }
