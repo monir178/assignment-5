b9571ff6d44cd2697da8ab4445d8b74345635d71
monir178 <monir.mzs17@gmail.com>
2022-08-23 19:56:54 +0600

    calculated total via javascript

--- js/calculate.js
+++ js/calculate.js
@@ -1,16 +1,47 @@
 document.getElementById('btn-calculate').addEventListener('click', function () {
+    playerCostTotalById();
+})
+
+document.getElementById('total-cost').addEventListener('click', function () {
+
     const playerPriceField = document.getElementById('player-price');
     const playerPriceValueString = playerPriceField.value;
     const playerPriceValue = parseInt(playerPriceValueString);
+    playerPriceField.value = '';
+    if (isNaN(playerPriceValue)) {
+        alert("Please Enter a Valid Data");
+        return;
+    }
 
     const totalPlayerCost = playerPriceValue * 5;
 
     const totalPlayerCostElement = document.getElementById('total-player-cost');
     totalPlayerCostElement.innerText = totalPlayerCost;
 
-})
+    const managerField = document.getElementById('manager-price');
+    const managerValueString = managerField.value;
+    const managerValue = parseInt(managerValueString);
+    managerField.value = '';
 
-document.getElementById('total-cost').addEventListener('click', function () {
-    const managerField = document.getElementById('')
+    if (isNaN(playerPriceValue)) {
+        alert("Please Enter a Valid Data");
+        return;
+    }
+
+    const coachField = document.getElementById('coach-price');
+    const coachValueString = coachField.value;
+    const coachValue = parseInt(coachValueString);
+    coachField.value = '';
+
+    if (isNaN(playerPriceValue)) {
+        alert("Please Enter a Valid Data");
+        return;
+    }
+
+
+    const totalCost = totalPlayerCost + managerValue + coachValue;
+
+    const totalCostElement = document.getElementById('all-cost');
+    totalCostElement.innerText = totalCost;
 })
 
--- js/utilities.js
+++ js/utilities.js
@@ -4,6 +4,8 @@ function selectPlayerById(playerNameId, playerBtnId) {
     const li = document.createElement('li');
 
 
+
+
     li.innerText = name.innerText;
     li.classList.add('item');
     addSelected.appendChild(li);
@@ -14,4 +16,27 @@ function selectPlayerById(playerNameId, playerBtnId) {
     btnName.setAttribute('disabled', true);
 
 
+}
+
+function playerCostTotalById() {
+    const playerPriceField = document.getElementById('player-price');
+    const playerPriceValueString = playerPriceField.value;
+    const playerPriceValue = parseInt(playerPriceValueString);
+    playerPriceField.value = '';
+
+    if (isNaN(playerPriceValue)) {
+        alert("Please Enter a Valid Data");
+        return;
+    }
+
+    let parentsList = document.querySelectorAll(".item");
+
+    let parentsLength = parentsList.length;
+
+    const totalPlayerCost = (parentsLength) * playerPriceValue;
+
+
+
+    const totalPlayerCostElement = document.getElementById('total-player-cost');
+    totalPlayerCostElement.innerText = totalPlayerCost;
 }
